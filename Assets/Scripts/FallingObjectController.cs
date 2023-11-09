6054db0fd3d8360f945bb66e50d060da2f234c78
ryuryu333 <risarisa062518@gmail.com>
2023-11-09 23:36:35 +0900

    FallingObjectの機能を実装
    スプライトのデータ管理の為にAddressablesパッケージをProjectにインストール
    画像素材作成
    スプライトをオブジェクトに反映させるコード作成

--- Assets/Scripts/FallingObjectController.cs
+++ Assets/Scripts/FallingObjectController.cs
@@ -1,42 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Xml.Schema;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class FallingObjectController : SingletonMonoBehaviour<FallingObjectController>
 {
+    [Header("インスペクタで値を指定")]
     [SerializeField] private GameObject fallingObjectPrefab;
     [SerializeField] private GameObject fallingObjectParent;
+    [SerializeField] private List<float> objcetScaleByTypeList = new();
+    [SerializeField] private List<AssetReferenceSprite> spriteRefList = new();
+    [SerializeField] private float generationIntervalTime;
+    [SerializeField] private float ignoreGameoverJudgmentTime;
+
+    //値のキャッシュ用変数
+    [Header("デバック用")]
+    [SerializeField] private List<Sprite> spriteList = new();
+    private List<Vector3> spriteScaleList = new();
+    private bool endInitialization = false;
     private Transform fallingObjectParentTransform;
     private List<GameObject> fallingObjectList = new();
     private List<StatusFallingObject> statusFallingObjectList = new();
-    [SerializeField] private float generationIntervalValue;
-    [SerializeField] private float generationIntervalTimer;
-    [SerializeField] private List<float> objcetScaleByTypeList = new List<float>();
+    private List<SpriteRenderer> fallingObjectSpriteRendererList = new();
     private int numberOfObjectType;
 
-    [SerializeField] private List<AssetReference> SpriteRefList = new();
-    [SerializeField] private List<Sprite> SpriteList = new();
-
+    [SerializeField] private float generationIntervalTimer;
     public float GenerationIntervalValue
-    { get { return generationIntervalValue; } set { generationIntervalValue = value; } }
+    { get => generationIntervalTime; set => generationIntervalTime = value; }
 
-    // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
+        string errorMassage = "インスペクター未記入";
+        if (fallingObjectPrefab == null || fallingObjectParent == null || objcetScaleByTypeList.Count == 0 || spriteRefList.Count == 0 || generationIntervalTime == 0) Debug.LogError(errorMassage);
+        //設定したスプライトを順に読み込み
+        for (int i = 0; i < spriteRefList.Count; i++)
+        {
+            //FallingObject用のスプライトを非同期でロード
+            AsyncOperationHandle<Sprite> handle = Addressables.LoadAssetAsync<Sprite>(spriteRefList[i]);
+            //ロード完了時に呼び出されるイベントを指定
+            handle.Completed += handle =>
+            {
+                if (handle.Status == AsyncOperationStatus.Failed) Debug.LogError("AssetReference failed to load.FallingObjcetSprite");
+                spriteList.Add(handle.Result);
+            };
+            await handle.Task;
+            //スプライトの大きさを取得し、FallingObjectにスプライトをアタッチした際に指定した大きさにできる拡大倍率を計算
+            float scaleX = objcetScaleByTypeList[i] / spriteList[i].bounds.size.x;
+            float scaleY = objcetScaleByTypeList[i] / spriteList[i].bounds.size.y;
+            spriteScaleList.Add(new Vector3(scaleX, scaleY, 1.0f));
+        }
+        //メンバ変数の初期化
         numberOfObjectType = objcetScaleByTypeList.Count;
         fallingObjectParentTransform = fallingObjectParent.transform;
-        generationIntervalTimer = generationIntervalValue;
+        generationIntervalTimer = generationIntervalTime;
+        //Tmpオブジェクトを削除
+        Destroy(GameObject.Find("Tmp"));        
+        endInitialization = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!endInitialization) return;
         CheckPlayerInput();
     }
 
@@ -49,18 +74,9 @@ public class FallingObjectController : SingletonMonoBehaviour<FallingObjectContr
         }
         if (Input.GetMouseButtonUp(0))
         {
-            generationIntervalTimer = generationIntervalValue;
+            generationIntervalTimer = generationIntervalTime;
             FallingObjectGenerate();
         }
-    }
-
-
-
-    private void Initialize()
-    {
-        numberOfObjectType = objcetScaleByTypeList.Count;
-        fallingObjectParentTransform = fallingObjectParent.transform;
-        generationIntervalTimer = generationIntervalValue;
     }
 
 
@@ -69,62 +85,68 @@ public class FallingObjectController : SingletonMonoBehaviour<FallingObjectContr
         //プレハブよりオブジェクトを複製
         Vector3 generatePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         generatePosition.z = 0; //z座標の取得値はカメラと同じ、カメラに映るようにz座標を変更
-        var instantiateObject = Instantiate(fallingObjectPrefab, generatePosition, Quaternion.identity, fallingObjectParentTransform);
+        var instantiateObject = Instantiate(fallingObjectPrefab, generatePosition, Quaternion.Euler(0, 0, Random.Range(0, 360)), fallingObjectParentTransform);
         //リストへ参照を保管
         fallingObjectList.Add(instantiateObject);
         StatusFallingObject statusInstantiateObject = instantiateObject.GetComponent<StatusFallingObject>();
-        statusFallingObjectList.Add(statusInstantiateObject);
+        statusFallingObjectList.Add(instantiateObject.GetComponent<StatusFallingObject>());
+        fallingObjectSpriteRendererList.Add(instantiateObject.GetComponent<SpriteRenderer>());
         //何個目に作成されたオブジェクトか書き込み、List[NumberFallingObject]番目で保管したリストへアクセス
         statusInstantiateObject.NumberFallingObject = statusFallingObjectList.Count - 1;
-        //タイプの決定、反映
-        int fallingObjectType = Random.Range(1, numberOfObjectType + 1);
-        ChangeFallingObjectStatus(statusInstantiateObject, fallingObjectType);
+        //タイプを乱数で決定、結果を反映
+        int fallingObjectType = Random.Range(0, numberOfObjectType - 3);
+        ChangeFallingObjectType(statusInstantiateObject, fallingObjectType);
     }
 
-    private void ChangeFallingObjectStatus(StatusFallingObject changedFallingObjectStatus, int newTypeValue)
+    private void ChangeFallingObjectType(StatusFallingObject changedFallingObjectStatus, int newTypeValue)
     {
         GameObject ChangedObject = changedFallingObjectStatus.gameObject;
-        //ステータスを変更
+        //ステータスを更新
         changedFallingObjectStatus.TypeFallingObject = newTypeValue;
-        //大きさを変更　ex. Type 1：1倍、Type2：0.7倍
-        float modifiedScale = objcetScaleByTypeList[newTypeValue - 1];
-        ChangedObject.transform.localScale = new Vector3(modifiedScale, modifiedScale, modifiedScale);
-        //オブジェクトに表示される数字を変更、将来的にはスプライトを差し替える処理に変更する
-        TextMeshProUGUI text = ChangedObject.transform.Find("Canvas/Text").GetComponent<TextMeshProUGUI>();
-        string objectName = (newTypeValue).ToString();
-        text.text = objectName;
+        //タイプに従ったスプライト、大きさに変更
+        fallingObjectSpriteRendererList[changedFallingObjectStatus.NumberFallingObject].sprite = spriteList[newTypeValue];
+        ChangedObject.transform.localScale = spriteScaleList[newTypeValue];
+        //コライダーを更新（重い処理なのでオブジェクトの数が多いと厳しいかも）
+        //コンポーネントを付けなおさないとコライダーがスプライトに沿った形に自動調整されない
+        Destroy(ChangedObject.GetComponent<PolygonCollider2D>());
+        ChangedObject.AddComponent<PolygonCollider2D>();
     }
 
     public void EventCollideFalilingObjects(StatusFallingObject collideObjectStatus, StatusFallingObject beCollidedObjectStatus)
     {
         //同じタイプのオブジェクトが触れた時のみ進化（ex. type 2 + type 2 -> type 1）
         if (collideObjectStatus.TypeFallingObject != beCollidedObjectStatus.TypeFallingObject) return;
-        //進化するオブジェクトを決定（もう片方のオブジェクトは非アクティブ化）
-        (StatusFallingObject beChangedObjectStatus, StatusFallingObject inactiveObjectStatus) collideObjectsstatuses = (null, null);
+        //進化させる、消去するオブジェクトを決定（上側にいるオブジェクトの方を進化させる）
+        bool collideObjectIsChanged = false;
         Vector3 collideObjectPosition = collideObjectStatus.gameObject.transform.position;
         Vector3 beCollidedObjectPosition = beCollidedObjectStatus.gameObject.transform.position;
-        if (collideObjectPosition.y == beCollidedObjectPosition.y)
+        bool isYPositionSame = collideObjectPosition.y == beCollidedObjectPosition.y;
+        if (isYPositionSame)
         {
-            if (collideObjectPosition.x > beCollidedObjectPosition.x) collideObjectsstatuses = (collideObjectStatus, beCollidedObjectStatus);
-            else collideObjectsstatuses = (beCollidedObjectStatus, collideObjectStatus);
+            bool isCollideObjectXPositionHigher = collideObjectPosition.x > beCollidedObjectPosition.x;
+            collideObjectIsChanged = isCollideObjectXPositionHigher ? true : false;
         }
         else
         {
-            if (collideObjectPosition.y > beCollidedObjectPosition.y) collideObjectsstatuses = (collideObjectStatus, beCollidedObjectStatus);
-            else collideObjectsstatuses = (beCollidedObjectStatus, collideObjectStatus);
+            bool isCollideObjectYPositionHigher = collideObjectPosition.y > beCollidedObjectPosition.y;
+            collideObjectIsChanged = isCollideObjectYPositionHigher ? true : false;
         }
+        StatusFallingObject beChangedObjectStatus = collideObjectIsChanged ? collideObjectStatus : beCollidedObjectStatus;
+        StatusFallingObject beNonActivedObjectStatus = collideObjectIsChanged ? beCollidedObjectStatus : collideObjectStatus;
         //進化
-        int currentTypeValue = collideObjectsstatuses.beChangedObjectStatus.TypeFallingObject;
-        if (currentTypeValue == 1)
+        int currentTypeValue = beChangedObjectStatus.TypeFallingObject;
+        int newTypeValue = currentTypeValue + 1;
+        if (newTypeValue == numberOfObjectType)
         {
-            //スコア加算等を追加する予定
-            collideObjectsstatuses.beChangedObjectStatus.gameObject.SetActive(false);
+            //スコア加算等を追加する予定、今は取り敢えず非アクティブ化（画面から消す）
+            beChangedObjectStatus.gameObject.SetActive(false);
         }
         else
         {
-            ChangeFallingObjectStatus(collideObjectsstatuses.beChangedObjectStatus, currentTypeValue - 1);
+            ChangeFallingObjectType(beChangedObjectStatus, newTypeValue);
         }
-        //進化元でないオブジェクトは非アクティブ化
-        collideObjectsstatuses.inactiveObjectStatus.gameObject.SetActive(false);
+        //進化させない方のオブジェクトは非アクティブ化（画面から消す）
+        beNonActivedObjectStatus.gameObject.SetActive(false);
     }
+
 }
